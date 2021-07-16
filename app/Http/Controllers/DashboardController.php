c2931eabf98b751aeedc0f62457f5f39a0fe3b3a
Theonlysmartboy <o2jose43@gmail.com>
2021-07-16 22:43:17 +0300

    Login checks

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     Public function master(Request $request){
+        
+        if(!Auth::check()){
+            return redirect('/login');
+        }
         if ($request->user()->hasRole('super')){
             $posts = Post::get();
             return view('super.dashboard',compact('posts'));
